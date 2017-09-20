4b5d5a10d2e1f7bf5234733675d0c34c7d9299d1
Дом <@DESKTOP-7GF1H2I>
2017-09-20 14:02:54 +0500

    решение

--- задание2/Program.cs
+++ задание2/Program.cs
@@ -10,6 +10,51 @@ namespace задание2
     {
         static void Main(string[] args)
         {
+            int M, N;
+            Console.WriteLine("Требуется ввести размер шахматной доски M x N ");
+            Console.WriteLine("Введите координату M");
+            Vvod("координата M = ", out M);
+            Proverka("координата M", ref M);
+            Console.WriteLine("Введите координату N");
+            Vvod("координата N = ", out N);
+            Proverka("координата N", ref N);
+
+            if (M == N) Console.WriteLine("Выиграет ПЕРВЫЙ игрок");
+            else
+                if ((M == 2) && (N == 3) || (M == 3) && (N == 2)) Console.WriteLine("Выиграет ВТОРОЙ игрок");
+            else if ((M % 2==0)&&(N % 2 == 0)) Console.WriteLine("Выиграет ВТОРОЙ игрок");
+            else Console.WriteLine("Выиграет ПЕРВЫЙ игрок");
+            Console.ReadKey();
+        }
+        static double Vvod(string s, out int n)//ввод числа
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = Int32.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
+        }
+        static void Proverka(string s, ref int a)//проверка ввода на отрицательность
+        {
+            bool ok = false;
+            string buf;
+            do
+            {
+                if (a > 0) ok = true;
+                else
+                {
+                    if (!ok) Console.WriteLine("\aВведите " + s + " заново");
+                    Console.Write(s + " = ");
+                    buf = Console.ReadLine();
+                    ok = Int32.TryParse(buf, out a);
+                    ok = false;
+                }
+            } while (!ok);
         }
     }
 }
